f2c0f3f107fde47ad81a67fe9ccfbe5f55fe4b72
Mustafa Sarshar <mustafa.sarshar88@gmail.com>
2022-08-13 21:18:09 +0200

    update

--- 03-Mini-Apps/JavaScript/11-Video-Player/index.js
+++ 03-Mini-Apps/JavaScript/11-Video-Player/index.js
@@ -1,6 +1,7 @@
 const btnWatchEL = document.querySelector(".btn")
 const btnCloseIcon = document.querySelector(".close-icon")
 const trailerContainerEl = document.querySelector(".trailer-container")
+const videoEl = document.querySelector("video")
 
 btnWatchEL.addEventListener("click", (event) => {
     trailerContainerEl.classList.remove("active")
@@ -8,4 +9,6 @@ btnWatchEL.addEventListener("click", (event) => {
 
 btnCloseIcon.addEventListener("click", (event) => {
     trailerContainerEl.classList.add("active")
+    videoEl.pause()
+    videoEl.currentTime = 0
 })
