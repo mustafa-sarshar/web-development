c7c7dae092610b6c549a4c8679d7194a7107a0f7
Mustafa Sarshar <mustafa.sarshar88@gmail.com>
2022-08-16 12:40:53 +0200

    update

---
+++ 01-Basics/TypeScript/01-DataTypes/src/index.ts
@@ -0,0 +1,13 @@
+console.log("Hello World")
+
+let age: number = 20
+
+if (age > 18)
+    console.log("You are an adult!")
+else
+    console.log("You must still listen to your parents!");
+
+function checkAdmin (userName:string): boolean {
+    if (userName === "Mustafa") return true
+    else return false
+}
---
+++ 01-Basics/TypeScript/README.md
@@ -0,0 +1,3 @@
+Thanks to:
+
+* [Programming with Mosh](https://www.youtube.com/watch?v=d56mG7DezGs) (accessed on 15.08.2022)
