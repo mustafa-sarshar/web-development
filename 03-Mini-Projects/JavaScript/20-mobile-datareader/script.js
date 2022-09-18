3617b451c451422e780e1568bb712599a8bf7706
mussar <musti.sar4@gmail.com>
2022-09-18 16:44:48 +0200

    update

--- 03-Mini-Projects/JavaScript/20-mobile-datareader/script.js
+++ 03-Mini-Projects/JavaScript/20-mobile-datareader/script.js
@@ -1,8 +1,15 @@
 const accelerometer = new Accelerometer({ frequency: 60 });
+const txtResultsEl = document.getElementById("txt-results");
 
 accelerometer.addEventListener("reading", (e) => {
   console.log(`Acceleration along the X-axis ${accelerometer.x}`);
   console.log(`Acceleration along the Y-axis ${accelerometer.y}`);
   console.log(`Acceleration along the Z-axis ${accelerometer.z}`);
+  txtResultsEl.innerHTML += `Acc-X: ${accelerometer.x}, Acc-Y: ${accelerometer.y}, Acc-Z: ${accelerometer.z}`;
 });
-accelerometer.start();
+
+
+window.onload = (event) => {
+    accelerometer.start();
+    txtResultsEl.innerHTML += " getting data ...";
+}
