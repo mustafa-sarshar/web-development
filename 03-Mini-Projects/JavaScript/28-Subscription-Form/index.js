95a902fb7b9cf59bd216b95f9e1cbb78a88514cd
musto <musti.sar4@gmail.com>
2023-06-07 11:46:30 +0200

    Add javascript example for FormData

---
+++ 03-Mini-Projects/JavaScript/28-Subscription-Form/index.js
@@ -0,0 +1,31 @@
+const txtUsernameEl = document.getElementById("username");
+const fileImageEl = document.getElementById("image");
+const imgPreviewContainerEl = document.getElementById("img-preview_container");
+const imgPreviewEl = document.getElementById("img-preview");
+const btnSubmitEl = document.getElementById("btn-submit");
+
+fileImageEl.addEventListener("change", (evt) => {
+  imgPreviewContainerEl.classList.add("hidden");
+
+  const file = evt.target.files[0];
+
+  if (file) {
+    const fileReader = new FileReader();
+    let fileData;
+
+    fileReader.onload = () => {
+      fileData = fileReader.result;
+      imgPreviewEl.setAttribute("src", fileData);
+      imgPreviewContainerEl.classList.remove("hidden");
+    };
+    fileReader.readAsDataURL(file);
+  }
+});
+
+btnSubmitEl.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  const fd = new FormData();
+  fd.append("username", txtUsernameEl.value);
+  fd.append("image", fileImageEl.value);
+  console.log(fd);
+});
