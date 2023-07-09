c2c8095cce759f9b63c7f5c9ee2f15df52d7210f
Mustafa Sarshar <mustafa.sarshar88@gmail.com>
2023-07-09 14:11:42 +0200

    add a new css project

--- 01-Basics/JavaScript/03_arrays.js
+++ 01-Basics/JavaScript/03_arrays.js
@@ -17,3 +17,14 @@ console.log(myArray);
 // Add an item to the first index of the array
 myArray.unshift("First item");
 console.log(myArray);
+
+const arrayFilled = myArray.fill(20, 3, 8);
+console.log(myArray, arrayFilled);
+
+// All 1 array
+const arr_ones = Array.from({ length: 10 }, () => 1);
+console.log(arr_ones);
+
+// Array 1 to 10
+const arr_1to10 = Array.from({ length: 10 }, (cur, i) => i + 1);
+console.log(arr_1to10);
