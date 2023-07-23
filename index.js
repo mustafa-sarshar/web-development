d93f89def44a93511163ca6f99d841274acdb706
mustafa-sarshar <mustafa.sarshar88@gmail.com>
2023-07-23 12:47:50 +0200

    Updates

--- index.js
+++ index.js
@@ -161,4 +161,117 @@ window.addEventListener("load", (_) => {
     document.getElementById("UserProximity").innerHTML =
       "UserProximity: " + event.near;
   });
+
+  // Extended Kalman Filter
+
+  function getOrientation(callback) {
+    window.addEventListener("deviceorientation", (event) => {
+      const alpha = event.alpha;
+      const beta = event.beta;
+      const gamma = event.gamma;
+      callback(alpha, beta, gamma);
+    });
+  }
+
+  function getAcceleration(callback) {
+    window.addEventListener("devicemotion", (event) => {
+      const acceleration = event.acceleration;
+      const x = acceleration.x;
+      const y = acceleration.y;
+      const z = acceleration.z;
+      callback(x, y, z);
+    });
+  }
+
+  const GRAVITY = 9.81;
+
+  function getPositionAndVelocity(callback) {
+    let alpha = 0;
+    let beta = 0;
+    let gamma = 0;
+    let x = 0;
+    let y = 0;
+    let z = 0;
+    let lastTimestamp = 0;
+
+    getOrientation((_alpha, _beta, _gamma) => {
+      alpha = _alpha;
+      beta = _beta;
+      gamma = _gamma;
+    });
+
+    getAcceleration((_x, _y, _z) => {
+      x = _x;
+      y = _y;
+      z = _z;
+      const timestamp = Date.now();
+      if (lastTimestamp !== 0) {
+        const dt = (timestamp - lastTimestamp) / 1000;
+        const ax = x * GRAVITY;
+        const ay = y * GRAVITY;
+        const az = z * GRAVITY;
+        const rotationMatrix = getRotationMatrix(alpha, beta, gamma);
+        const acceleration = rotateVector([ax, ay, az], rotationMatrix);
+        const velocity = [0, 0, 0];
+        const position = [0, 0, 0];
+        for (let i = 0; i < 3; i++) {
+          velocity[i] += acceleration[i] * dt;
+          position[i] += velocity[i] * dt;
+        }
+        callback(position, velocity);
+      }
+      lastTimestamp = timestamp;
+    });
+  }
+
+  function getRotationMatrix(alpha, beta, gamma) {
+    const degToRad = Math.PI / 180;
+    const phi = gamma * degToRad;
+    const theta = beta * degToRad;
+    const psi = alpha * degToRad;
+    const cosPhi = Math.cos(phi);
+    const sinPhi = Math.sin(phi);
+    const cosTheta = Math.cos(theta);
+    const sinTheta = Math.sin(theta);
+    const cosPsi = Math.cos(psi);
+    const sinPsi = Math.sin(psi);
+    const R11 = cosPhi * cosPsi - sinPhi * cosTheta * sinPsi;
+    const R12 = cosPhi * sinPsi + sinPhi * cosTheta * cosPsi;
+    const R13 = sinPhi * sinTheta;
+    const R21 = -sinPsi * cosTheta;
+    const R22 = cosPsi * cosTheta;
+    const R23 = sinTheta;
+    const R31 = sinPhi * cosPsi + cosPhi * cosTheta * sinPsi;
+    const R32 = sinPhi * sinPsi - cosPhi * cosTheta * cosPsi;
+    const R33 = cosPhi * sinTheta;
+    return [
+      [R11, R12, R13],
+      [R21, R22, R23],
+      [R31, R32, R33],
+    ];
+  }
+
+  function rotateVector(vector, matrix) {
+    const result = [0, 0, 0];
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        result[i] += vector[j] * matrix[i][j];
+      }
+    }
+    return result;
+  }
+
+  setInterval(() => {
+    if (is_running) {
+      getPositionAndVelocity((position, velocity) => {
+        updateFieldIfNotNull("Position_x", position[0]);
+        updateFieldIfNotNull("Position_y", position[1]);
+        updateFieldIfNotNull("Position_z", position[2]);
+
+        updateFieldIfNotNull("Velocity_x", velocity[0]);
+        updateFieldIfNotNull("Velocity_y", velocity[1]);
+        updateFieldIfNotNull("Velocity_z", velocity[2]);
+      });
+    }
+  }, 1000);
 });
